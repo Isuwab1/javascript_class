d3238397a46cd6aa1bbf5dba56dfc1526fbc24a0
Ahmed <ahmedmahmud1994@hotmail.com>
2026-04-02 22:31:39 +0100

    import/export, function, ternary

---
+++ class_notes/import_export.js
@@ -0,0 +1,4 @@
+import { add } from "./function.js";
+
+let result = add(2, 3)
+console.log(result); // output the result - this will output 5
--- class_notes/ternary.js
+++ class_notes/ternary.js
@@ -4,4 +4,33 @@ if (score > 30) {
     console.log("You passed the exam.");
 } else {
     console.log("You failed the exam.");
-} 
+}
+
+// ternary operator - a shorthand way of writing an if/else statement
+score > 30 ? console.log("You passed the exam.") : console.log("You failed the exam.");
+
+// condition ? expressionIfTrue : expressionIfFalse
+
+// if (day === "Sunday") {
+//     console.log("Tomorrow is Monday.");    
+// } else if (day === "Monday") {
+//     console.log("Today is Tuesday.");
+// } else if (day === "Tuesday") {
+//     console.log("Today is not Monday or Tuesday.");
+// } else if (day === "Wednesday") {
+//     console.log("Today is Wednesday.");
+// }   else if (day === "Thursday") {
+//     console.log("Today is Thursday.");
+// } else if (day === "Friday") {
+//     console.log("Today is Friday.");
+// } else if (day === "Saturday") {
+//     console.log("Today is Saturday.");
+// }else {
+//     console.log("Invalid day.");
+// }
+
+let day = "monday";
+
+day === " tuesday" ? console.log("today is tuesday.")
+    : day === "Monday" ? console.log("Tomorrow is Tuesday.")
+        : console.log("Tomorrow is not Monday or Tuesday.");
